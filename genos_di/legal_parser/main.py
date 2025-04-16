dab8a6a0d743039353f0b0d8c808f6b1918c746f
smreosms13 <smreosms13@gmail.com>
2025-04-16 05:03:11 +0000

    feat: add parallel processing and improve logging

--- genos_di/legal_parser/main.py
+++ genos_di/legal_parser/main.py
@@ -1,11 +1,10 @@
-from typing import Optional
 
-from fastapi import Depends, FastAPI, Query
-
-from params import AdmRuleRequestParams, APIEndpoints, LawItemRequestParams
-from schemas import ParserResult
-from service import fetch_api, get_parse_result
 import logging
+
+from fastapi import Depends, FastAPI
+
+from params import AdmRuleRequestParams, LawItemRequestParams
+from service import download_data, get_parse_result
 
 logging.basicConfig(level=logging.INFO,
                     format='[%(name)s]-%(levelname)s: %(message)s')
@@ -19,23 +18,23 @@ app = FastAPI()
 async def read_root():
     return "Hello, Parser for Legal Data"
 
-###  Data Parsing
-
-# TODO 법령 \ 행정규칙으로 엑셀 파일 가져와서 ID("법률일련번호"), LID(행정규칙일련번호) 추출
 # TODO 하루에 한 번 돌아가게 만들기
 
-# KEY = 법률일련번호(MST) / 행정규칙일련번호(ID)
 @app.get("/metadata-example")
-async def parser_metadata(KEY: Optional[str] = Query("264627")) -> list[ParserResult]:
-    return await get_parse_result(KEY)
+async def parser_metadata() -> int:
+    law_ids_dict = {
+        "law_ids": ["224537", "264627", "268423"],
+        "admrule_ids": ["2100000213205"]
+    }
+    return await get_parse_result(law_ids_dict)
 
-### 기타
+
+### OPEN API 법령 정보 원본 (JSON) 다운로드
 @app.get("/law-download")
 async def download_law(query: LawItemRequestParams = Depends()):
-    api_url = APIEndpoints().get_full_url(query.get_query_params())
-    return await fetch_api(api_url)
+    await download_data(query)
+    
 
 @app.get("/admrule-download")
 async def download_admrule(query: AdmRuleRequestParams = Depends()):
-    api_url = APIEndpoints().get_full_url(query.get_query_params())
-    return await fetch_api(api_url)
+    await download_data(query)

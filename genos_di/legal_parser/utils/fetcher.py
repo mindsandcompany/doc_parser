32fb841bad86de06f669237bbd76bdc7da16400b
smreosms13 <smreosms13@gmail.com>
2025-04-21 09:11:39 +0000

    refactor:  Move API call to fetcher and update export_json in file_utils

---
+++ genos_di/legal_parser/utils/fetcher.py
@@ -0,0 +1,55 @@
+import logging
+from typing import Union
+
+import aiohttp
+from fastapi import status
+
+from params import (
+    AdmBylRequestParams,
+    AdmRuleRequestParams,
+    APIEndpoints,
+    LawItemRequestParams,
+    LawSystemRequestParams,
+    LicBylRequestParams,
+)
+from utils.exception_handler import ClientError
+
+logger = logging.getLogger(__name__)
+
+## API GET Request
+async def fetch_api(id:str, url: str):
+    async with aiohttp.ClientSession() as client:
+        async with client.get(url) as response:
+            content_type = response.headers.get("Content-Type", "")
+            
+            if response.status == 200:
+                if "application/json" in content_type:
+                    data = await response.json()
+                    if "Law" in data.keys():
+                        raise ClientError(id=id, detail="해당되는 법령/행정데이터를 찾을 수 없음 : {url}")
+                else:
+                    data = await response.text()
+                    logger.warning(f"[fetch_api] 예상치 못한 데이터 타입: {content_type} ({url})")
+                    raise ClientError(id=id, detail=f"Unexpected content type: {url}: {content_type}", status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+            else:
+                logger.error(f"[fetch_api] API 요청 실패: {url} (HTTP {response.status})")
+                raise ClientError(id=id, detail=f"Request {url} failed with status {response.status}", status_code=status.HTTP_400_BAD_REQUEST)
+            
+            return data
+        
+# API 호출 
+async def get_api_response(
+    id:str,
+    query: Union[
+        LawItemRequestParams,
+        LawSystemRequestParams,
+        LicBylRequestParams,
+        AdmBylRequestParams,
+        AdmRuleRequestParams,
+    ],
+):
+    api_url = APIEndpoints().get_full_url(query.get_query_params())
+    logger.info(f"[get_api_response] API 요청 시작: {api_url}")
+    response = await fetch_api(id, api_url)
+    logger.info(f"[get_api_response] API 요청 성공: {api_url}")
+    return response

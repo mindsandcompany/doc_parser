5f56727dbb3e471e7862d45ff4b18bc82fb84b9c
smreosms13 <smreosms13@gmail.com>
2025-04-21 09:25:20 +0000

    fix: Add API for test and use loader keys from csv file

--- genos_di/legal_parser/main.py
+++ genos_di/legal_parser/main.py
@@ -1,10 +1,12 @@
 
 import logging
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Query
 
 from params import AdmRuleRequestParams, LawItemRequestParams
+from schemas import ParserResponse
 from service import download_data, get_parse_result
+from utils.file_utils import load_keys_from_csv
 
 logging.basicConfig(level=logging.INFO,
                     format='[%(name)s]-%(levelname)s: %(message)s')
@@ -20,13 +22,22 @@ async def read_root():
 
 # TODO 하루에 한 번 돌아가게 만들기
 
-@app.get("/metadata-example")
-async def parser_metadata() -> int:
-    law_ids_dict = {
-        "law_ids": ["224537", "264627", "268423"],
-        "admrule_ids": ["2100000213205"]
+@app.get("/test")
+async def test_parser(
+    i_start:int = Query(default=0),
+    i_end:int = Query(default=5)
+) -> ParserResponse:
+    law_ids_dict = load_keys_from_csv()
+    law_ids_input = {
+        "law_ids": law_ids_dict.get('law_ids', [])[i_start:i_end],
+        "admrule_ids": law_ids_dict.get('admrule_ids', [])[i_start:i_end]
     }
-    return await get_parse_result(law_ids_dict)
+    return await get_parse_result(law_ids_input)
+
+@app.get("/parser")
+async def run_parser() -> ParserResponse:
+    law_ids_input = load_keys_from_csv()
+    return await get_parse_result(law_ids_input)
 
 
 ### OPEN API 법령 정보 원본 (JSON) 다운로드

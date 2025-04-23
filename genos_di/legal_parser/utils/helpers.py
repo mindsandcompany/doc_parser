2747134d1c8cf2cc7a2d142f34cd28c914300368
smreosms13 <smreosms13@gmail.com>
2025-04-23 08:39:11 +0000

    chore(helpers): update renamed function

--- genos_di/legal_parser/utils/helpers.py
+++ genos_di/legal_parser/utils/helpers.py
@@ -6,6 +6,6 @@ def replace_strip(data: list[str]) -> list[str]:
     return [x.strip() for x in data if x.strip()]
 
 def format_date(year:str, month:str, day:str) -> str:
-    if type_converter._is_valid(tuple(year, month, day), tuple[str, str, str]):
+    if type_converter.validator((year, month, day), tuple[str, str, str]):
         return f"{year}{int(month):02d}{int(day):02d}"        
        

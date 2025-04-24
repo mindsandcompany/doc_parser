c24323bf31f8ff201ee8027fe90af08c50923f61
smreosms13 <smreosms13@gmail.com>
2025-04-24 07:36:39 +0000

    remove: exception_handler

--- genos_di/legal_parser/utils/exception_handler.py
+++
@@ -1,9 +0,0 @@
-from fastapi import status
-
-
-class ClientError(Exception):
-    def __init__(self, id:str, detail: str, status_code: int = status.HTTP_404_NOT_FOUND):
-        self.id = id 
-        self.detail = detail
-        self.status_code = status_code
-    

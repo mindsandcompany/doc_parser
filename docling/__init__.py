1bf13acf5e1272b25ce5665245a185d4a93529d7
namseunghyun <seunghyun.nam@genon.ai>
2025-09-09 17:35:26 +0900

    facade init 제거

--- docling/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Docling package for document processing"""
-
-from .facade import DocumentFacade
-from .facade.config import ProcessorConfig, ProcessorMode
-
-__version__ = "1.0.0"
-__all__ = ['DocumentFacade', 'ProcessorConfig', 'ProcessorMode']
